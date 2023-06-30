2964f5917d6c8c94cf3c10ccca535eb993f8f018
Hannah Kolbeck <hskolbeck@gmail.com>
2023-06-30 00:11:49 -0700

    Flesh out sampler a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use core::marker::PhantomData;
+
 pub trait Hal<T> {
     fn read(&mut self) -> T;
     fn timestamp_ns(&self) -> u64;
@@ -8,20 +10,26 @@ pub trait Hal<T> {
 pub struct Sampler<T: 'static, H: Hal<T>> {
     hal: H,
     delay_ns: u64,
-    __phantom_data: T,
+    __phantom_data: PhantomData<T>,
 }
 
 impl<T, H: Hal<T>> Sampler<T, H> {
-    pub fn new(hal: H, buf: &mut [T], sample_rate_hz: u32) -> Sampler<T, H> {
-        todo!()
+    pub fn new(hal: H, sample_rate_hz: u32) -> Sampler<T, H> {
+        let delay_ns = 1_000_000_000 / sample_rate_hz as u64;
+        Sampler {
+            hal,
+            delay_ns,
+            __phantom_data: PhantomData::default(),
+        }
     }
 
-    pub fn sample_frame<F: FnMut(&mut [T], u64)>(mut self, mut buf: &mut [T], fun: F) -> ! {
+    pub fn sample_frame<F: FnMut(&mut [T], u64)>(mut self, mut buf: &mut [T], mut fun: F) -> ! {
         let mut batch_start = self.hal.timestamp_ns();
         let mut last_read = 0;
         let mut idx = 0;
         loop {
             if self.hal.timestamp_ns() <= last_read + self.delay_ns || last_read == 0 {
+                last_read = self.hal.timestamp_ns();
                 buf[idx] = self.hal.read();
                 idx += 1;
 
