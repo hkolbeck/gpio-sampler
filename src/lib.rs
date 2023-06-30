92d02368aac39ecafcafe113c7dcabc3ddfa92b3
Hannah Kolbeck <hskolbeck@gmail.com>
2023-06-30 00:04:30 -0700

    Sketch of sampler

--- src/lib.rs
+++ src/lib.rs
@@ -5,37 +5,31 @@ pub trait Hal<T> {
     fn timestamp_ns(&self) -> u64;
 }
 
-pub struct Sampler<T: 'static, H: Hal<T>, F: FnMut(&mut [T], u64)> {
+pub struct Sampler<T: 'static, H: Hal<T>> {
     hal: H,
-    buf: &'static mut [T],
-    fun: F,
     delay_ns: u64,
+    __phantom_data: T,
 }
 
-impl <T, H: Hal<T>, F: FnMut(&mut [T], u64)> Sampler<T, H, F> {
-    pub fn new() -> Sampler<T, H, F> {
+impl<T, H: Hal<T>> Sampler<T, H> {
+    pub fn new(hal: H, buf: &mut [T], sample_rate_hz: u32) -> Sampler<T, H> {
         todo!()
     }
 
-    pub fn sample(mut self) -> ! {
+    pub fn sample_frame<F: FnMut(&mut [T], u64)>(mut self, mut buf: &mut [T], fun: F) -> ! {
         let mut batch_start = self.hal.timestamp_ns();
-        let mut last_read = batch_start;
-
+        let mut last_read = 0;
         let mut idx = 0;
-        self.buf[idx] = self.hal.read();
-        idx += 1;
-
         loop {
-            if self.hal.timestamp_ns() <= last_read + self.delay_ns {
-                if idx >= self.buf.len() {
-                    (self.fun)(&mut self.buf, self.hal.timestamp_ns() - batch_start);
+            if self.hal.timestamp_ns() <= last_read + self.delay_ns || last_read == 0 {
+                buf[idx] = self.hal.read();
+                idx += 1;
+
+                if idx >= buf.len() {
+                    fun(&mut buf, self.hal.timestamp_ns() - batch_start);
                     batch_start = self.hal.timestamp_ns();
                     idx = 0
                 }
-
-                self.buf[idx] = self.hal.read();
-                last_read = self.hal.timestamp_ns();
-                idx += 1;
             }
         }
     }
